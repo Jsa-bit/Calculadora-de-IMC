47deed0ee48f26b985a82e9ca0ee385bc397d1a7
Jsa-bit <jefersonsimonett@gmail.com>
2024-04-27 16:37:43 -0300

    Projeto finalizado
    Calculadora de IMC responsiva finalizada.

--- js/script.js
+++ js/script.js
@@ -5,17 +5,38 @@ form.addEventListener('submit',function(event){
 
     const weight = document.getElementById('weight').value;
     const height = document.getElementById('height').value;
-    const variavel_Ponto = parseFloat('height');
 
     const bmi = (weight/(height*height)).toFixed(2);
     const value = document.getElementById('value');
 
     let description = ''
 
+    value.classList.add('attention');
+
     document.getElementById('infos').classList.remove('hidden');
 
-    if (bmi < 18.5) (
+    if (bmi < 18.5) {
         description = 'Você está abaixo do peso ideal.'
-    )
+    }else if (bmi >= 18.5  && bmi <= 25){
+        description= "Você está no peso ideal.";
+        value.classList.remove('attention');
+        value.classList.add('normal');
+    }else if (bmi > 25 && bmi<= 30) {
+        description = "Cuidado,você está com sobrepeso."
+    }else if (bmi > 30 && bmi <= 35){
+        description = "Cuidado,você está com obesidade moderada."
+    }else if (bmi > 35 && bmi <= 40){
+        description = "Cuidado,você está com obesidade severa."
+    }else {
+        description = "Você tem obesidade mórbida."
+    }
+        
+    
+
+
+
+
+
+    value.textContent = bmi.replace('.',',');
     document.getElementById('description').textContent=description;
 });
